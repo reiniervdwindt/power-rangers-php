d5ef929c00be36aa8e673f29168ecfee74a1aa48
Reinier van der Windt <reiniervdwindt@gmail.com>
2016-04-09 16:13:37 +0200

    FIX - Fixed recording of http requests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,6 @@
 require 'vendor/autoload.php';
 \VCR\VCR::configure()
     ->enableLibraryHooks(['curl'])
+    ->enableRequestMatchers(array('method', 'url', 'host'))
     ->setStorage('json');
 \VCR\VCR::turnOn();
